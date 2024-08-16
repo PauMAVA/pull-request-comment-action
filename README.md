007d8e11f230380d6ee37e97176bc06d0b5ff318
PauMAVA <paumachetti@gmail.com>
2024-08-16 09:01:08 +0200

    Improve action

---
+++ README.md
@@ -0,0 +1,74 @@
+# pull-request-comment-action
+Look for a "trigger word" in a pull-request description or comment, so that later steps can know whether or not to run.
+
+## Credit
+Based on the abandoned action repo: https://github.com/Khan/pull-request-comment-trigger/tree/master
+
+## Example usage
+Your workflow needs to listen to the following events:
+```
+on:
+  pull_request:
+    types: [opened]
+  issue_comment:
+    types: [created]
+```
+
+And then you can use the action in your jobs like this:
+
+```
+jobs:
+  deploy:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: shanegenschaw/pull-request-comment-trigger@v2.1.0
+        id: check
+        with:
+          trigger: '@deploy'
+          reaction: rocket
+        env:
+          GITHUB_TOKEN: '${{ secrets.GITHUB_TOKEN }}'
+      - run: 'echo Found it!'
+        if: steps.check.outputs.triggered == 'true'
+```
+
+You can either pass arguments in your comment, e.g. `@deploy dev`:
+
+```
+jobs:
+  deploy:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: shanegenschaw/pull-request-comment-trigger@v2.1.0
+        id: check
+        with:
+          trigger: '@deploy **'
+          reaction: rocket
+          allow_arguments: true
+        env:
+          GITHUB_TOKEN: '${{ secrets.GITHUB_TOKEN }}'
+      - run: 'echo Found it! Deploy on ${{ fromJson(steps.check.outputs.arguments)[0] }}'
+        if: steps.check.outputs.triggered == 'true'
+```
+
+Reaction must be one of the reactions here: https://developer.github.com/v3/reactions/#reaction-types
+And if you specify a reaction, you have to provide the `GITHUB_TOKEN` variable.
+
+## Inputs
+
+| Input | Required? | Description |
+| ----- | --------- | ----------- |
+| trigger | Yes | The string to look for in pull-request descriptions and comments. For example "#build/android". |
+| trigger | No | The user that must be mentioned in the PR. |
+| prefix_only | No (default 'false') | If 'true', the trigger must match the start of the comment. |
+| reaction | No (default '') | If set, the specified emoji "reaction" is put on the comment to indicate that the trigger was detected. For example, "rocket". |
+| allow_arguments | No (default 'false') | If 'true', script looks for `**` markers that are considered as comment arguments. |
+
+
+## Outputs
+
+| Output | Description |
+| ------ | ----------- |
+| triggered | 'true' or 'false' depending on if the trigger phrase was found. |
+| comment_body | The comment body. |
+| arguments | The comment arguments. |
---
+++ index.js
@@ -0,0 +1,115 @@
+#!/usr/bin/env node
+
+const core = require("@actions/core");
+const { context, getOctokit } = require("@actions/github");
+
+async function run() {
+    // Basic argument handling
+    const trigger = core.getInput("trigger", { required: true });
+    const mention = core.getInput("mention", { required: false });
+
+    if (mention && !mention.startsWith("@")) {
+        mention = "@" + mention;
+    }
+
+    const reaction = core.getInput("reaction");
+    const { GITHUB_TOKEN } = process.env;
+    if (reaction && !GITHUB_TOKEN) {
+        core.setFailed('If "reaction" is supplied, GITHUB_TOKEN is required');
+        return;
+    }
+
+    // Get body of the comment
+    const body =
+        (context.eventName === "issue_comment"
+            ? // For comments on pull requests
+              context.payload.comment.body
+            : // For the initial pull request description
+              context.payload.pull_request.body) || "";
+    core.setOutput("comment_body", body);
+
+    // In case the comment is not pull-request based, abort.
+    if (
+        context.eventName === "issue_comment" &&
+        !context.payload.issue.pull_request
+    ) {
+        // not a pull-request comment, aborting
+        core.setOutput("triggered", "false");
+        return;
+    }
+
+    // Check if the trigger text is in the body
+    const { owner, repo } = context.repo;
+
+    const prefixOnly = core.getInput("prefix_only") === "true";
+    const allowArguments = core.getInput("allow_arguments") === "true";
+
+    // If no mention is needed then we default to true in order to trigger next steps.
+    let hasMention = mention ? body.contains(mention) : true;
+    let hasTrigger = body.conatins(trigger);
+
+    if (allowArguments) {
+        let regexRawTrigger = trigger.replace(/\s\*{2}/g, " [^\\s]+");
+
+        if (prefixOnly) {
+            regexRawTrigger = `^${regexRawTrigger}$`;
+        } else {
+            regexRawTrigger = `${regexRawTrigger}$`;
+        }
+
+        const regexTrigger = new RegExp(regexRawTrigger);
+
+        hasTrigger = regexTrigger.test(body);
+    }
+
+    if ((prefixOnly && !hasTrigger) || !hasTrigger || !hasMention) {
+        core.setOutput("triggered", "false");
+        return;
+    }
+
+    // At his point the trigger check has passed.
+    core.setOutput("triggered", "true");
+
+    // Parse arguments and set them as output
+    if (allowArguments && trigger.includes("**")) {
+        const args = [];
+
+        const triggerSplit = trigger.split(" ");
+        const bodySplit = body.split(" ");
+
+        triggerSplit.forEach((part, i) => {
+            if (part !== "**") return;
+
+            args.push(bodySplit[i]);
+        });
+
+        core.setOutput("arguments", JSON.stringify(args));
+    }
+
+    // Create reaction in the comment if needed
+    if (!reaction) {
+        return;
+    }
+
+    const client = getOctokit(GITHUB_TOKEN);
+    if (context.eventName === "issue_comment") {
+        await client.rest.reactions.createForIssueComment({
+            owner,
+            repo,
+            comment_id: context.payload.comment.id,
+            content: reaction,
+        });
+    } else {
+        await client.rest.reactions.createForIssue({
+            owner,
+            repo,
+            issue_number: context.payload.pull_request.number,
+            content: reaction,
+        });
+    }
+}
+
+run().catch((err) => {
+    console.error(err);
+    core.setFailed("Unexpected error");
+});
